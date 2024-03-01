c64fa3baa683fa81cfff7e8a9d95483794b04756
saikat2912 <saikat2270@gmail.com>
2024-03-01 20:54:58 +0530

    Starter commit

--- demo/demo/src/main/java/com/example/demo/StarterController.java
+++ demo/demo/src/main/java/com/example/demo/StarterController.java
@@ -1,4 +1,21 @@
 package com.example.demo;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+
 public class StarterController {
+
+    @GetMapping("/hello")
+    public String Hello(){
+        return "Hello";
+    }
+
+    @PostMapping("/hello")
+    public String helloPost(@RequestBody String name){
+        return "hello "+name+"!";
+    }
 }
---
+++ knowurcompany/knowurcompany/src/main/java/com/example/knowurcompany/job/Job.java
@@ -0,0 +1,21 @@
+package com.example.knowurcompany.job;
+
+public class Job {
+
+    private Long id;
+    private String title;
+    private String description;
+    private String minSalary;
+
+    private String maxSalary;
+    private String location;
+
+    public Job(Long id, String title, String description, String minSalary, String maxSalary, String location) {
+        this.id = id;
+        this.title = title;
+        this.description = description;
+        this.minSalary = minSalary;
+        this.maxSalary = maxSalary;
+        this.location = location;
+    }
+}
